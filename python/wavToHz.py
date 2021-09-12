5cf9fd7f1d842f30fc4d665d2657067b5a5b4078
Henrynaut <neilmchenry@gmail.com>
2021-09-12 00:19:21 -0500

    Created main.py and fixed import bugs

--- python/wavToHz.py
+++ python/wavToHz.py
@@ -3,44 +3,50 @@ from aubio import source, pitch
 import numpy as np
 # import matplotlib
 
-your_file = "DejaVu.wav"
-samplerate = 4100
-win_s = 4096
-hop_s = 512 
+# class wavToHz:
+#     def __init__(self, your_file, samplerate):
+#         self.your_file = "nothing"
+#         self.samplerate = 4100
 
-s = source(your_file, samplerate, hop_s)
-samplerate = s.samplerate
+def convert(filename):
 
-tolerance = 0.8
+    win_s = 4096
+    hop_s = 512 
+    samplerate = 4100
 
-pitch_o = pitch("yin", win_s, hop_s, samplerate)
-pitch_o.set_unit("midi")
-pitch_o.set_tolerance(tolerance)
+    s = source(filename, samplerate, hop_s)
+    samplerate = s.samplerate
 
-pitches = []
-confidences = []
+    tolerance = 0.8
 
-total_frames = 0
-while True:
-    samples, read = s()
-    pitch = pitch_o(samples)[0]
-    pitches += [pitch]
-    confidence = pitch_o.get_confidence()
-    confidences += [confidence]
-    total_frames += read
-    if read < hop_s: break
+    pitch_o = pitch("yin", win_s, hop_s, samplerate)
+    pitch_o.set_unit("midi")
+    pitch_o.set_tolerance(tolerance)
 
-print(pitch)
+    pitches = []
+    confidences = []
 
-# Calculate duration
+    total_frames = 0
+    while True:
+        samples, read = s()
+        pitch = pitch_o(samples)[0]
+        pitches += [pitch]
+        confidence = pitch_o.get_confidence()
+        confidences += [confidence]
+        total_frames += read
+        if read < hop_s: break
 
-import wave
-import contextlib
-fname = '/tmp/test.wav'
-with contextlib.closing(wave.open(fname,'r')) as f:
-    frames = f.getnframes()
-    rate = f.getframerate()
-    duration = frames / float(rate)
-    print(duration)
+    print(pitch)
+
+    # Calculate duration
+
+    import wave
+    import contextlib
+    fname = '/tmp/test.wav'
+    with contextlib.closing(wave.open(fname,'r')) as f:
+        frames = f.getnframes()
+        rate = f.getframerate()
+        duration = frames / float(rate)
+        print(duration)
 
 # print("Average frequency = " + str(np.array(pitches).mean()) + " hz")
